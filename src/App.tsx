9a6f802d6a6ca5993ba86b6bd78309fcfaff68f5
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-16 20:32:55 -0800

    cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import viteLogo from "/vite.svg";
-import { createSignal } from "solid-js";
+import { createSignal, For } from "solid-js";
 import solidLogo from "./assets/solid.svg";
-import "./App.css";
+// import "./App.css";
 import * as v from "@badrap/valita";
 import { createReconnectingWS } from "@solid-primitives/websocket";
+import { franc } from "franc-all";
 
 const Message = v.object({
     commit: v.object({
@@ -12,15 +13,26 @@ const Message = v.object({
         }),
     }),
 });
-type Message = v.Infer<typeof Message>;
+
+interface Data {
+    text: string;
+    words: string[];
+}
 
 const ws = createReconnectingWS("wss://jetstream2.us-west.bsky.network/subscribe?wantedCollections=app.bsky.feed.post");
-const [message, setMessage] = createSignal<Message | undefined>();
+const [data, setData] = createSignal<Data | undefined>();
+
+const segmenter = new Intl.Segmenter("eng", { granularity: "word" });
+
 ws.addEventListener("message", (ev) => {
     if (typeof ev.data === "string") {
         try {
             const m = Message.parse(JSON.parse(ev.data), { mode: "passthrough" });
-            setMessage(m);
+            const text = m.commit.record.text;
+            const language = franc(text);
+            if (language !== "eng") return;
+            const words = [...segmenter.segment(text)].map((v) => v.segment.trim()).filter((v) => !!v);
+            setData({ text, words });
         } catch {}
     }
 });
@@ -30,8 +42,13 @@ function App() {
         <>
             <div class="card">
                 <p>
-                    {message()?.commit.record.text}
+                    {data()?.text}
                 </p>
+                <ul>
+                    <For each={data()?.words}>
+                        {(item) => <li>{item}</li>}
+                    </For>
+                </ul>
             </div>
         </>
     );
