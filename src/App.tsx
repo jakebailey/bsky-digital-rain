a767f9c516fd73e5bc6d47095b4e18305f2e21b0
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-17 09:02:48 -0800

    cool

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { For } from "solid-js";
+import { createSignal, For } from "solid-js";
 // import "./App.css";
 import * as v from "@badrap/valita";
 import { createPolled } from "@solid-primitives/timer";
 import { createReconnectingWS } from "@solid-primitives/websocket";
 import { franc } from "franc-all";
 import englishWords from "most-common-words-by-language/build/resources/english.txt?raw";
-import { createStore } from "solid-js/store";
 
 const commonWords = new Set(englishWords.split("\n").slice(0, 100));
 
@@ -19,15 +18,27 @@ const Message = v.object({
     }),
 });
 
-const segmenter = new Intl.Segmenter("eng", { granularity: "word" });
+const segmenters = new Map<string, Intl.Segmenter>();
+function getSegmenter(language: string) {
+    let segmenter = segmenters.get(language);
+    if (!segmenter) {
+        segmenter = new Intl.Segmenter(language, { granularity: "word" });
+        segmenters.set(language, segmenter);
+    }
+    return segmenter;
+}
 
 const ws = createReconnectingWS("wss://jetstream2.us-west.bsky.network/subscribe?wantedCollections=app.bsky.feed.post");
 
-// const [words, setWords] = createStore<Record<string, number>>({});
+interface WordCount {
+    word: string;
+    count: number;
+}
 
-// const words: Record<string, number> = {};
+const words = new Map<string, WordCount>();
+const wordsByCount: WordCount[] = [];
 
-let recentWords: { words: Record<string, number>; timestamp: number; }[] = [];
+let messageCount = 0;
 
 ws.addEventListener("message", (ev) => {
     if (typeof ev.data !== "string") return;
@@ -35,57 +46,86 @@ ws.addEventListener("message", (ev) => {
     try {
         const m = Message.parse(JSON.parse(ev.data), { mode: "passthrough" });
         const text = m.commit.record.text;
+        messageCount++;
         const language = franc(text);
         if (language !== "eng") return;
+        const segmenter = getSegmenter(language);
 
-        const newWordsMap: Record<string, number> = {};
+        const newWordsMap = new Map<string, number>();
         for (const segment of segmenter.segment(text)) {
             const word = segment.segment.trim().toLowerCase();
-            if (!word || isIgnored.test(word) || commonWords.has(word)) continue;
-            // newWordsMap[word] = (newWordsMap[word] ?? 0) + 1;
-            newWordsMap[word] = 1; // Count each word only once per message
+            if (word.length <= 0 || isIgnored.test(word)) continue;
+
+            const count = newWordsMap.get(word) ?? 0;
+            newWordsMap.set(word, count + 1);
+
+            // newWordsMap.set(word, 1); // Count each word only once per message
         }
 
-        recentWords.push({
-            words: newWordsMap,
-            timestamp: Date.now(),
-        });
-
-        // setWords((prev) => {
-        //     const next = { ...prev };
-        //     for (const word in newWordsMap) {
-        //         next[word] = (next[word] ?? 0) + newWordsMap[word];
-        //     }
-        //     return next;
-        // });
-
-        // for (const word in newWordsMap) {
-        //     words[word] = (words[word] ?? 0) + newWordsMap[word];
-        // }
+        for (const [word, count] of newWordsMap) {
+            const existing = words.get(word);
+            if (existing) {
+                existing.count += count;
+            } else {
+                const wc: WordCount = { word, count };
+                words.set(word, wc);
+                wordsByCount.push(wc);
+            }
+        }
     } catch {}
 });
 
-// const sortedWords = createPolled(() => Object.entries(words).sort((a, b) => b[1] - a[1]).slice(0, 50), 1000);
+const useInsertionSort = false;
 
-const sortedWords = createPolled(() => {
-    const words: Record<string, number> = {};
-    for (const { words: recent } of recentWords) {
-        for (const word in recent) {
-            words[word] = (words[word] ?? 0) + recent[word];
-        }
+function sort<T>(arr: T[], compareFn: (a: T, b: T) => number): T[] {
+    if (!useInsertionSort) {
+        return arr.sort(compareFn);
     }
 
-    const idx = recentWords.findIndex((r) => Date.now() - r.timestamp < 1000 * 60 * 5);
-    if (idx >= 0) recentWords = recentWords.slice(idx);
-    return Object.entries(words).sort((a, b) => b[1] - a[1]).slice(0, 50);
-}, 200);
+    // Hand-written insertion sort could be better for nearly sorted data...
+    for (let i = 1; i < arr.length; i++) {
+        let key = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && compareFn(arr[j], key) > 0) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+
+        arr[j + 1] = key;
+    }
+
+    return arr;
+}
+
+const data = createPolled(() => {
+    const before = performance.now();
+
+    const sortedWords = sort(wordsByCount, (a, b) => b.count - a.count).slice(0, 50).map((word) =>
+        [word.word, word.count] as const
+    );
+
+    const after = performance.now();
+
+    return {
+        messageCount,
+        sortedWords,
+        time: after - before,
+    };
+}, 250);
 
 function App() {
     return (
         <>
+            <p>
+                {data().messageCount} messages processed
+            </p>
+            <p>
+                {data().time.toFixed(2)} ms to sort
+            </p>
             <div class="card">
                 <ul>
-                    <For each={sortedWords()}>
+                    <For each={data().sortedWords}>
                         {([word, count]) => <li>{word} - {count}</li>}
                     </For>
                 </ul>
